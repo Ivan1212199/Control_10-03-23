bf629f0737c656f049bff4a01b7e52158bab8ff6
Ivan <ivan18-18@example.com>
2023-03-13 10:59:18 +0300

    закоончил с задачей

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] ArrayOne = new string[5] {"123", "23", "hello", "world", "res"};
+string[] ArrayTwo = new string[ArrayOne.Length];
+void SecondArrayWithIF(string[] ArrayOne, string[] ArrayTwo)
+{
+    for (int i = 0; i  <ArrayOne.Length; i++)
+    {
+    if(ArrayOne[i].Length  <= 3)
+        {
+        ArrayTwo[i] = ArrayOne[i];
+        i++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for    (int i = 0;   i<ArrayOne.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(ArrayOne, ArrayTwo);
+PrintArray(ArrayTwo);
